4d3600b6a4d255503769e3d49e911ac0411a1f72
andrewokeh <35554217+andrewokeh@users.noreply.github.com>
2024-08-07 19:33:52 -0700

    Update Reverse Linked List.py
    Add recursive solution

--- Linked Lists/Reverse Linked List.py
+++ Linked Lists/Reverse Linked List.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 
-Iterative solution
+# Iterative solution
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
@@ -18,6 +18,15 @@ class Solution:
         return prev
     
 # Recursive solution
-# class Solution:
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-# TBA...
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        new_head = head
+        if head.next:
+            new_head = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return new_head
