2bd0e2656a1e88f83ecb6bf67d69768e24cb4714
Andrew <andrewokeh@outlook.com>
2024-09-18 18:45:53 -0700

    Add leetcode 704 solution

---
+++ Binary_Search/Binary_Search.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/binary-search/
+# 704. Binary Search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                return m
+
+        return -1
